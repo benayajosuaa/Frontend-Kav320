ff336db5242e7cc4271867d4e00ff59a7d1fbc49
benaya josua <benaya.josua@gmail.com>
2026-05-01 22:31:15 +0700

    update : update navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -8,37 +8,35 @@ const quesFont = Questrial({
   weight: "400",
 });
 
-
-
-
-
 export default function NavigationBar() {
-
-    
-    return (
-        <div className={` ${quesFont.className} bg-transparent`}>
-            {/* pembagian */}
-            <div className="grid grid-cols-2 items-center pt-10 pb-3 px-15">
-                {/* kiri */}
-                <div className="bg-b">
-                    <Link href="/">
-                    <img src="/logo/kamarlogo.png" className="h-15 w-auto" />
-                    </Link>
-                </div>
-
-                {/* kanan */}
-                <div className="">
-                    <div className="flex justify-start">
-                        <div className="flex border border-black divide-x divide-black">
-                            <button className="p-2 w-40">
-                                <Link href="/work">Work</Link>
-                            </button>
-                            <button className="p-2 w-40">Service & Payment</button>
-                            <button className="p-2 w-40">Contact</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={`${quesFont.className} bg-transparent`}>
+      {/* pembagian */}
+      <div className="grid grid-cols-2 items-start px-15 pt-9 pb-0 bg-transparent">
+        {/* kiri */}
+        <div className="bg-transparent">
+          <Link href="/">
+            <img src="/logo/kamarlogo.png" className="h-15 w-auto" />
+          </Link>
         </div>
-    );
+
+        {/* kanan */}
+        <div className="bg-transparent">
+          <div className="grid grid-cols-4 auto-rows-[2.75rem] content-start gap-0 bg-transparent">
+            <Link href="/work" className="flex h-11 text-lg justify-center items-center border-l border-t border-b border-black px-1 bg-transparent">
+                Work
+            </Link>
+            <Link href="/services" className="flex h-11 text-lg justify-center items-center border-t border-r border-b border-l border-black px-1 bg-transparent">
+                Service
+            </Link>
+            <Link href="/contact" className="flex h-11 text-lg justify-center items-center border-t border-r border-b border-black px-1 bg-transparent">
+                Relation
+            </Link>
+            <div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
