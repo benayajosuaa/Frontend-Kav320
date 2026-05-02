f73e3d33f0c3dfec253ab288073faddfd7ad4699
benaya josua <benaya.josua@gmail.com>
2026-05-02 18:48:21 +0700

    update : perbaikan homepage

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -1,9 +1,16 @@
 import Navbar from "../../../components/navbar"
 import Footer from "../../../components/footer"
+import { Questrial } from "next/font/google";
+
+const quesFont = Questrial({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function HomePage() {
   return (
-    <div className="relative">
+    <div className={`${quesFont.className} bg-transparent`}>
+        {/* SECTION: NAVBAR */}
         <div className="fixed inset-x-0 top-0 z-30">
             <Navbar />
         </div>
@@ -13,7 +20,34 @@ export default function HomePage() {
                 <h1>Testing</h1>
             </div>
         </div>
+        
+        {/* Garis */}
+        <div className="ml-15 mr-15 border-[#838383] border border-b"/>
 
+        {/* SECTION: KEBERLANJUTAN */}
+        <div className="bg-green-50">
+            <div className="h-screen">
+                <div className="p-25">
+                    {/* Judul */}
+                    <div>
+                        <span>
+                            <h1 className="text-6xl">Keberlanjutan Kami</h1>
+                        </span>
+                        <span className="flex pt-10">
+                            <p className="text-3xl">
+                                kamar 320 bukanlah suatu projek yang bersifat temporal <br/>
+                                melainkan suatu ambisi berkelanjutan yang akan menghadiran pelayanan holistik <br/>
+                                yang membantu masyarakat untuk mendapatkan kebutuhan teknologi yang memadai
+                            </p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+        {/* SECTION: FOOTER */}
         <div>
             <Footer />
         </div>
