4c85c159922456ff885d2284b6c9f46188281ab2
benaya josua <benaya.josua@gmail.com>
2026-05-05 12:45:54 +0700

    update : tambahin responsive design

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -14,60 +14,121 @@ const quesFont = Questrial({
 
 export default function NavigationBar() {
     return (
-        <div className={`relative isolate overflow-hidden pointer-events-none ${quesFont.className} h-screen bg-transparent`}>
-            {/* Section 1 */}
-            <div>
-                {/* pembagian */}
-                <div className="grid grid-cols-2 items-center px-15 pt-9 pb-0">
-                    {/* kiri */}
-                    <div className=""/>
-                    {/* kanan */}
-                    <div className="">
-                        <div className="grid grid-cols-4 gap-0 divide-y divide-black ">
-                            <div className="bg-transparent h-11"/>
-                            <div className="bg-transparent h-11"/>
-                            <div className="bg-transparent h-11"/>
-                            <div className="bg-transparent h-11"/>
-                            <div className="flex h-11 items-center border-l  border-b border-black px-1 justify-center ">
-                                <p className="text-lg ">Phone</p>
+        <div className={`${quesFont.className}`}>
+            {/* DESKTOP */}
+            <div className="hidden md:block">
+                <div className={`relative isolate overflow-hidden pointer-events-none h-screen bg-gray-50`}>
+                {/* Section 1 */}
+                <div>
+                    {/* pembagian */}
+                    <div className="grid grid-cols-2 items-center px-15 pt-9 pb-0">
+                        {/* kiri */}
+                        <div className=""/>
+                            {/* kanan */}
+                            <div className="">
+                                <div className="grid grid-cols-4 gap-0 divide-y divide-black ">
+                                    <div className="bg-transparent h-11"/>
+                                    <div className="bg-transparent h-11"/>
+                                    <div className="bg-transparent h-11"/>
+                                    <div className="bg-transparent h-11"/>
+                                    <div className="flex h-11 items-center border-l  border-b border-black px-1 justify-center ">
+                                        <p className="text-lg ">Phone</p>
+                                    </div>
+                                    <div className="pl-5 p-2 col-span-3 flex h-11 items-center  border-r border-b border-l border-black px-1">
+                                        <p className="text-lg">+62 897-0052-654</p>
+                                    </div>
+                                    <div className="flex h-11 items-center justify-center border-l  border-b border-black px-1">
+                                        <p className="text-lg">Email</p>
+                                    </div>
+                                    <div className="pl-5 p-2  col-span-3 flex h-11 items-center  border-r  border-l border-black px-1">
+                                        <p className="text-lg">
+                                            contact@kamar320.com
+                                        </p>
+                                    </div>
+                                    <div className="bg-transparent h-11 border-0"/>
+                                    <div className="pl-5 p-2 col-span-3 flex h-auto items-center  border-r border-b border-l border-black px-1">
+                                        <p className="text-lg">
+                                            Jl. Boulevard M.H. Thamrin 1100, <br/> Lippo Village, Kelapa Dua, <br/> Tangerang, Banten <br/>Indonesia  
+                                            <br/>
+                                            <span className="text-sm italic">*alamat bisa berubah kapan saja</span>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="pl-5 p-2 col-span-3 flex h-11 items-center  border-r border-b border-l border-black px-1">
-                                <p className="text-lg">+62 897-0052-654</p>
-                            </div>
-                            <div className="flex h-11 items-center justify-center border-l  border-b border-black px-1">
-                                <p className="text-lg">Email</p>
-                            </div>
-                            <div className="pl-5 p-2  col-span-3 flex h-11 items-center  border-r  border-l border-black px-1">
-                                <p className="text-lg">
-                                    contact@kamar320.com
-                                </p>
-                            </div>
-                            <div className="bg-transparent h-11 border-0"/>
-                            <div className="pl-5 p-2 col-span-3 flex h-auto items-center  border-r border-b border-l border-black px-1">
-                                <p className="text-lg">
-                                    Jl. Boulevard M.H. Thamrin 1100, <br/> Lippo Village, Kelapa Dua, <br/> Tangerang, Banten <br/>Indonesia  
-                                    <br/>
-                                    <span className="text-sm italic">*alamat bisa berubah kapan saja</span>
-                                </p>
-                            </div>
+                        </div>
+                    </div>
+                    
+                    {/* Section 2*/}
+                    <div className="absolute bottom-0 bg-transparent p-15">
+                        <div>
+                            <h1 className="text-8xl">Kecakapan pelayanan profesional dan responsif</h1>
+                        </div>
+                        <div className="pt-10">
+                            <h4 className="text-xl">
+                                <span>2026 © kamar320  - </span>
+                                <span>develop by Benaya Joshua, part of </span>
+                                <span className="font-underline">halobenaya.com</span>
+                            </h4>
                         </div>
                     </div>
                 </div>
             </div>
-            
-            {/* Section 2*/}
-            <div className="absolute bottom-0 bg-transparent p-15">
-                <div>
-                    <h1 className="text-8xl">Kecakapan pelayanan profesional dan responsif</h1>
-                </div>
-                <div className="pt-10">
-                    <h4 className="text-xl">
-                        <span>2026 © kamar320  - </span>
-                        <span>develop by Benaya Joshua, part of </span>
-                        <span className="font-underline">halobenaya.com</span>
-                    </h4>
+
+
+            <div className="md:hidden">
+                <div className="p-5">
+                    <div className="flex flex-col gap-y-3">
+                        <div>
+                            <h1 className="text-3xl">Kecakapan pelayanan profesional dan responsif</h1>
+                        </div>
+
+                        <div className="pb-6">
+                            <div className="">
+                                <div className="grid grid-cols-4 gap-0 divide-y divide-black ">
+                                    
+                                    <div className="flex h-11 items-center  border-b border-t border-black px-1 justify-center ">
+                                        <p className="text-sm ">Phone</p>
+                                    </div>
+                                    <div className="pl-5 p-1 border-t col-span-3 flex h-11 items-center  border-b  border-black px-1">
+                                        <p className="text-sm">+62 897-0052-654</p>
+                                    </div>
+                                    <div className="flex h-11 items-center justify-center   border-b border-black px-1">
+                                        <p className="text-sm">Email</p>
+                                    </div>
+                                    <div className="pl-5 p-1  col-span-3 flex h-11 items-center   border-black px-1">
+                                        <p className="text-sm">
+                                            contact@kamar320.com
+                                        </p>
+                                    </div>
+                                    <div className="bg-transparent h-11 border-0"/>
+                                    <div className="pl-5 p-2 col-span-3 flex h-auto items-center border-l border-black px-1">
+                                        <p className="text-sm">
+                                            Jl. Boulevard M.H. Thamrin 1100, <br/> Lippo Village, Kelapa Dua, <br/> Tangerang, Banten <br/>Indonesia  
+                                            <br/>
+                                            <span className="text-sm italic">*alamat bisa berubah kapan saja</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="pb-10">
+                            <div className="flex items-center justify-center">
+                                <h4 className="text-sm">
+                                    <span>2026 © kamar320  - </span>
+                                    <span>develop by Benaya Joshua, part of </span>
+                                    <span className="font-underline">halobenaya.com</span>
+                                </h4>
+                            </div>
+                        </div>
+
+
+
+                    </div>
                 </div>
             </div>
+
+
         </div>
     );
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { RxHamburgerMenu } from "react-icons/rx";
 import Link from "next/link";
 import { Questrial } from "next/font/google";
 
@@ -11,29 +11,52 @@ const quesFont = Questrial({
 export default function NavigationBar() {
   return (
     <div className={`${quesFont.className} bg-transparent`}>
-      {/* pembagian */}
-      <div className="grid grid-cols-2 items-start px-15 pt-9 pb-0 ">
-        {/* kiri */}
-        <div className="bg-transparent">
-          <Link href="/">
-            <img src="/logo/kamarlogo.png" className="h-15 w-auto" />
-          </Link>
-        </div>
-
-        {/* kanan */}
-        <div className="bg-transparent">
-          <div className="grid grid-cols-4 auto-rows-[2.75rem] content-start gap-0 bg-transparent">
-            <Link href="/work" className="flex h-11 text-lg justify-center items-center border-l border-t border-b border-black px-1 bg-transparent">
-                Work
-            </Link>
-            <Link href="/services" className="flex h-11 text-lg justify-center items-center border-t border-r border-b border-l border-black px-1 bg-transparent">
-                Service
-            </Link>
-            <Link href="/contact" className="flex h-11 text-lg justify-center items-center border-t border-r border-b border-black px-1 bg-transparent">
-                Relation
-            </Link>
-            <div>
+      {/* Tampilan Desktop */}
+      <div className="hidden md:block">
+         <div className="w-full">
+          {/* pembagian */}
+          <div className="grid grid-cols-2 items-start px-15 pt-9 pb-0 ">
+            {/* kiri */}
+            <div className="">
+              <Link href="/" className="bg-transparent">
+                <img src="/logo/kamarlogo.png" className="h-15 w-auto" />
+              </Link>
             </div>
+
+            {/* kanan */}
+            <div className="">
+              <div className="grid grid-cols-4 auto-rows-[2.75rem] content-start gap-0 bg-transparent">
+                <Link href="/work" className="bg-linear-to-b from-white/50 to-white/70 backdrop-blur-md flex h-11 text-lg justify-center items-center border-l border-t border-b border-black px-1 bg-transparent">
+                    Work
+                </Link>
+                <Link href="/services" className="bg-linear-to-b from-white/50 to-white/70 backdrop-blur-md flex h-11 text-lg justify-center items-center border-t border-r border-b border-l border-black px-1 bg-transparent">
+                    Service
+                </Link>
+                <Link href="/contact" className="bg-linear-to-b from-white/50 to-white/70 backdrop-blur-md flex h-11 text-lg justify-center items-center border-t border-r border-b border-black px-1 bg-transparent">
+                    Relation
+                </Link>
+                <div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      {/* Tampilan Mobile */}
+
+      <div className="md:hidden">
+        <div className="flex flex-row justify-between items-center p-5 pt-6 bg-transparent">
+          {/* Logo */}
+          <div>
+            <img className="h-11 w-auto" src="/logo/kamarlogo.png" alt="logo" />
+          </div>
+
+          {/* Hamburger Menu */}
+          <div className="text-lg cursor-pointer border border-black pl-10 pr-10 p-1">
+            <h1>menu</h1>
           </div>
         </div>
       </div>
