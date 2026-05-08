809d3bb580376c686a4877214ee7bef44a7104cd
benaya josua <benaya.josua@gmail.com>
2026-05-08 21:51:13 +0700

    update : update tonase warna loker

--- src/app/(public)/loker/page.tsx
+++ src/app/(public)/loker/page.tsx
@@ -30,21 +30,21 @@ export default function HomePage(){
             
             {/* Section Loker DEKSTOP*/}
             <div>
-                <div className="w-full bg-amber-50">
+                <div className="w-full bg-[#e7e5df]">
 
                     
                     <div className="relative w-full overflow-hidden pt-20 p-3 md:p-10 md:pt-30">
                         {/* Loker Content */}
                         <div className="relative z-20">
                             <div className="grid grid-cols-2 auto-rows-[140px] gap-1 md:auto-rows-[165px] lg:grid-cols-4 2xl:grid-cols-5">
-                                <div className="col-span-2 row-span-4 md:row-span-3 lg:row-span-2 border-slate-400 border-4 bg-gray-100 p-4 md:p-5">
+                                <div className="col-span-2 row-span-4 md:row-span-3 lg:row-span-2 border-[#8aa0bd] border-4 bg-gray-100 p-4 md:p-5">
                                     <div className="flex h-full flex-col justify-between gap-y-6 md:gap-y-10">
                                         {/* row 1 */}
                                         <div>
                                             <div className="f">
                                                 <div className="">
                                                     <div className={silkscreen.className}>
-                                                        <div className="w-full max-w-90 border-[#7b7b7b] border-6 bg-[#3d3d3d] p-2">
+                                                        <div className="w-full max-w-90 border-[#7b7b7b] border-6 bg-[#3a3a3a] p-2">
                                                             <span>
                                                                 <TextType 
                                                                     text={[
@@ -59,7 +59,7 @@ export default function HomePage(){
                                                                     cursorCharacter="|"
                                                                     deletingSpeed={50}
                                                                     cursorBlinkDuration={0.5}
-                                                                    className="text-[#ffee00]"
+                                                                    className="text-[#e8d23c]"
                                                                 />
                                                             </span>
                                                         </div>
@@ -89,7 +89,7 @@ export default function HomePage(){
                                             </div>
                                         </div>
                                         {/* row 3 */}
-                                        <div className="mt-auto flex w-full flex-col gap-y-2 border-t border-slate-300 pt-3 md:pt-4">
+                                        <div className="mt-auto text-[#3b6ea8] flex w-full flex-col  border-t border-slate-300 pt-3 md:pt-4">
                                             <div>
                                                 <h1 className="pb-1 text-lg font-semibold md:pb-2 md:text-xl">butuh bantuan ?</h1>
                                             </div>
