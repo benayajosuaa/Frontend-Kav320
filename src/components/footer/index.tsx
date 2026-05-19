eed17740893bf0d084c72d6787a223f376df3583
benaya josua <benaya.josua@gmail.com>
2026-05-19 16:20:50 +0700

    update : update design dari footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import Link from "next/link";
 import { Questrial } from "next/font/google";
 
@@ -8,9 +7,17 @@ const quesFont = Questrial({
   weight: "400",
 });
 
+const navigationLink = [
+    { href: "/work", label: "Kerjaan kamar320" },
+    { href: "/services", label: "Pelayanan" },
+    { href: "/contact", label: "Relasi" },
+]
 
 
-
+const sosialMedia = [
+    { href: "/https://wa.me/628970052654", label: "Whatsapp"}, 
+    { href: "https://instagram.com/benayajosuaa", label: "Instagram"}, 
+]
 
 export default function NavigationBar() {
     return (
@@ -74,66 +81,80 @@ export default function NavigationBar() {
                 </div>
             </div>
 
-
+            {/* MOBILE */}
             <div className="md:hidden" data-navbar-theme="dark">
-                <div className="p-5">
-                    <div className="flex flex-col gap-y-3">
-                        <div className="border border-gray-200"/>
-                        {/* Logo Pintu */}
+                <div className="p-8 pt-20 bg-[#4B4B4B]">
+                   <div className="flex flex-col gap-y-6 text-white">
+                        
                         <div>
-                            <div className="h-20 w-30 bg-amber-200">
-
-                            </div>
+                            <img 
+                                className="h-12 w-auto"
+                                src="logo/icon-pintu.png" alt="icon-kamar320" 
+                            />
                         </div>
-                        <div>
-                            <h1 className="text-3xl">Kecakapan pelayanan profesional dan responsif</h1>
-                        </div>
-
-                        <div className="pt-5 pb-10">
-                            <div className="">
-                                <div className="grid grid-cols-4 gap-0 divide-y divide-[#ffffff] ">
-                                    
-                                    <div className="flex h-11 items-center  border-b border-t border-[#ffffff] px-1 justify-center ">
-                                        <p className="text-sm ">Phone</p>
-                                    </div>
-                                    <div className="pl-5 p-1 border-t col-span-3 flex h-11 items-center  border-b  border-[#ffffff] px-1">
-                                        <p className="text-sm">+62 897-0052-654</p>
-                                    </div>
-                                    <div className="flex h-11 items-center justify-center   border-b border-[#ffffff] px-1">
-                                        <p className="text-sm">Email</p>
-                                    </div>
-                                    <div className="pl-5 p-1  col-span-3 flex h-11 items-center  border-[#ffffff] px-1">
-                                        <p className="text-sm">
-                                            contact@kamar320.com
-                                        </p>
-                                    </div>
-                                    <div className="bg-transparent h-11 border-0"/>
-                                    <div className="pl-5 p-2 col-span-3 flex h-auto items-center border-l border-[#ffffff] px-1">
-                                        <p className="text-sm">
-                                            Jl. Boulevard M.H. Thamrin 1100, <br/> Lippo Village, Kelapa Dua, <br/> Tangerang, Banten <br/>Indonesia  
-                                            <br/>
-                                            <span className="text-sm italic">*alamat bisa berubah kapan saja</span>
-                                        </p>
-                                    </div>
+                        <div className="flex flex-row">
+                            {/* Social Media */}
+                            <div className="flex basis-1/2 flex-col">
+                                <div className="pb-2">
+                                    <h1 className="text-lg text-[#ababab] font-semibold">Sosial</h1>
                                 </div>
-                            </div>
+                                <div className="gap-y-1 flex flex-col border-l pl-2">
+                                    {
+                                        sosialMedia.map((x)=> {
+                                            return (
+                                                <div key={x.href}>
+                                                    <Link
+                                                        key={x.href} 
+                                                        href={x.href}
+                                                        target="blank"
+                                                        >
+                                                            {x.label}
+                                                    </Link>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div> 
+                            {/* Media sosial */}
+                            <div className="flex basis-1/2 flex-col">
+                                <div className="pb-3">
+                                    <h1 className="text-lg text-[#ababab] font-semibold">Link</h1>
+                                </div>
+                                <div className="gap-y-1 flex flex-col border-l pl-2">
+                                    {
+                                        navigationLink.map((x) => {
+                                            return (
+                                                <div key={x.href}>
+                                                    <Link
+                                                        href={x.href}
+                                                    >
+                                                        {x.label}
+                                                    </Link>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div> 
                         </div>
 
-                        <div className="pb-10">
-                            <div className="flex items-center justify-center text-center">
-                                <h4 className="text-sm">
-                                    <span>2026 © kamar320  - </span>
-                                    <span>develop by Benaya Joshua </span>
-                                    <span>part of </span>
-                                    <span className="underline">halobenaya.com</span>
-                                </h4>
-                            </div>
+                        <div>
+                            <h1 className="text-sm pt-10">
+                                Jl. Boulevard M.H. Thamrin 1100, <br/> Lippo Village, Kelapa Dua, <br/> Tangerang, Banten <br/>Indonesia 
+                            </h1>
                         </div>
-                    </div>
+
+                        <div>
+                            <h4 className="text-sm text-center pt-10 pb-10">
+                                <span>2026 © kamar320  - </span>
+                                <span>develop by Benaya Joshua, part of </span>
+                                <span className="font-underline">halobenaya.com</span>
+                            </h4>
+                        </div>
+                   </div>
                 </div>
             </div>
-
-
         </div>
     );
 }
