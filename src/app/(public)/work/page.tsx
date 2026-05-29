cc63098c25129bcbccab5e812c3cac420fa6a9b6
benaya josua <benaya.josua@gmail.com>
2026-05-29 18:13:02 +0700

    update : update work page

--- src/app/(public)/work/page.tsx
+++ src/app/(public)/work/page.tsx
@@ -15,7 +15,7 @@ const quesFont = Questrial({
 const listWork = [
     {   
         index:1,
-        title:"Judul 1",
+        title:"Photoscape",
         gambar:"work/photoscape.png",
         link_demo:"https://instagram.com/benayajosuaa",
         magazine:"https://instagram.com/benayajosuaa",
@@ -23,7 +23,7 @@ const listWork = [
     },
     {
         index:2,
-        title:"Judul 2",
+        title:"Personal Portfolio",
         gambar:"work/photoscape.png",
         link_demo:"https://instagram.com/benayajosuaa",
         magazine:"https://instagram.com/benayajosuaa",
@@ -31,19 +31,11 @@ const listWork = [
     },
     {
         index:3,
-        title:"Judul 3",
+        title:"Vector Database for Recipe",
         gambar:"work/photoscape.png",
         link_demo:"https://instagram.com/benayajosuaa",
         magazine:"https://instagram.com/benayajosuaa",
         type:"Website"
-    },
-    {
-        index:4,
-        title:"Judul 4",
-        gambar:"work/photoscape.png",
-        link_demo:"https://instagram.com/benayajosuaa",
-        magazine:"https://instagram.com/benayajosuaa",
-        type:"Design"
     },
 ]
 
@@ -80,51 +72,51 @@ export default function HomePage(){
                 </div>
 
 
-                <div className="p-15">
+                <div className="px-4 py-12 sm:px-8 sm:py-14 md:px-10 lg:p-15">
                     <div className="flex flex-col">
                         {/* Judul */}
                         <div>
-                            <div className="pt-24 sm:pt-28 sm:pb-6 md:pt-30 md:pb-15 lg:pt-20 ">
-                                <h1 className="text-3xl font-bold text-[#5F2E6D] sm:text-4xl md:text-[2.7rem] lg:text-4xl">yang sudah dikerjakan kamar320</h1>
+                            <div className="pt-14 pb-8 sm:pt-20 sm:pb-10 md:pt-28 md:pb-12 lg:pt-20 lg:pb-0">
+                                <h1 className="max-w-4xl text-3xl font-bold leading-tight text-[#5F2E6D] sm:text-4xl md:text-[2.7rem] lg:text-4xl">yang sudah dikerjakan kamar320</h1>
                             </div>
                         </div>
                         {/* Section */}
-                        <div className="flex flex-col gap-y-5">
+                        <div className="pt-10 flex flex-col gap-y-10 sm:gap-y-12 lg:gap-y-8">
                             {
                                 listWork.map((x) => {
                                     return (
-                                        <div key={x.index} className="flex flex-col gap-y-7">
-                                            <div>
-                                                <div className="flex flex-row">
-                                                    <div className="flex basis-4/10">
-                                                        <div className="flex h-40 w-80 bg-amber-300">
+                                        <div key={x.index} className="flex flex-col gap-y-7 sm:gap-y-7 lg:gap-y-7">
+                                            <div className="border-b border-[#d0d0d0] pb-8 sm:pb-10 lg:pb-8">
+                                                <div className="flex flex-col gap-y-4 md:flex-row md:gap-x-8 lg:gap-x-0">
+                                                    <div className="flex w-full md:basis-4/10 lg:basis-4/10">
+                                                        <div className="flex aspect-16/10 w-full overflow-hidden bg-amber-300 md:h-44 lg:h-40 lg:w-80 lg:aspect-auto">
                                                             <img 
-                                                                className="h-40 w-80 object-cover object-[center_0%] border border-gray-200"
+                                                                className="h-full w-full border border-gray-200 object-cover object-[center_0%] lg:h-40 lg:w-80"
                                                                 src={x.gambar} alt="" 
                                                             />
                                                         </div>
                                                     </div>
-                                                    <div className="flex basis-6/10">
-                                                        <div className="flex flex-col justify-between">
+                                                    <div className="flex w-full md:basis-6/10 lg:basis-6/10">
+                                                        <div className="flex w-full flex-col justify-between gap-y-5 md:min-h-44 lg:min-h-0 lg:gap-y-0">
                                                             <div>
-                                                                <h1 className="text-2xl font-semibold">
+                                                                <h1 className="text-2xl font-semibold leading-tight sm:text-3xl md:text-2xl lg:text-2xl">
                                                                     {x.title}
                                                                 </h1>
                                                             </div>
-                                                            <div  className="flex flex-row gap-x-3 items-center">
+                                                            <div  className="flex flex-wrap items-center gap-x-2 md:gap-x-5 gap-y-2 lg:flex-nowrap">
                                                                 <div>
-                                                                    <h1 className="text-lg font-semibold">
+                                                                    <h1 className="text-base font-semibold sm:text-lg">
                                                                         {x.type}
                                                                     </h1>
                                                                 </div>
                                                                 <span className="font-extrabold text-[#d0d0d0]"> | </span>
-                                                                <div className="text-lg font-semibold">
+                                                                <div className="text-base font-semibold underline-offset-4 hover:underline sm:text-lg">
                                                                     <Link href={x.magazine}>
                                                                         Majalah Project
                                                                     </Link>
                                                                 </div>
                                                                 <span className="font-extrabold text-[#d0d0d0]"> | </span>
-                                                                <div className="text-lg font-semibold"> 
+                                                                <div className="text-base font-semibold underline-offset-4 hover:underline sm:text-lg"> 
                                                                     <Link href={x.link_demo}>
                                                                         Demo Project
                                                                     </Link>
@@ -134,7 +126,7 @@ export default function HomePage(){
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="border-b border-[#d0d0d0]"/>
+                                            
                                         </div>
                                     )
                                 })
