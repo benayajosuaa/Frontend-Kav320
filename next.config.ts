9e6fea31ce416a6eb56ea1cb204d9313ba35b54d
benaya josua <benaya.josua@gmail.com>
2026-05-31 10:56:40 +0700

    update : add marsada uph

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/marsada-uph",
+        destination:
+          "https://drive.google.com/drive/folders/1pzaP4dMcxzXxGZ_msXGhtHN_p5q4u2tg?usp=sharing",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
